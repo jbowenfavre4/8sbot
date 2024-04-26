655601ce7b0adbab6c62d3b7e3a0983681ef9b18
Bowen Revill <bowen.revill@vivint.com>
2024-04-26 09:55:12 -0600

    troubleshooting

--- start_app.sh
+++ start_app.sh
@@ -3,6 +3,8 @@
 # Change directory to where your package.json is located
 cd 8sbot
 
+cd ls
+
 # Retrieve Parameter Store values directly in the deployment script
 BOT_TOKEN=$(aws ssm get-parameter --name /jr/bot_token --query Parameter.Value --output text)
 CLIENT_ID=$(aws ssm get-parameter --name /jr/client_id --query Parameter.Value --output text)
