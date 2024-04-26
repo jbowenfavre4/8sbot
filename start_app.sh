37b0a7ec7220ef30180975b8cc3d2f6e4eef9ee8
Bowen Revill <bowen.revill@vivint.com>
2024-04-26 10:11:47 -0600

    i wonder

--- start_app.sh
+++ start_app.sh
@@ -3,15 +3,19 @@
 # Change directory to where your package.json is located
 cd 8sbot
 
-cd ls
-
 # Retrieve Parameter Store values directly in the deployment script
 BOT_TOKEN=$(aws ssm get-parameter --name /jr/bot_token --query Parameter.Value --output text)
 CLIENT_ID=$(aws ssm get-parameter --name /jr/client_id --query Parameter.Value --output text)
 SERVER_ID=$(aws ssm get-parameter --name /jr/server_id --query Parameter.Value --output text)
 
+export BOT_TOKEN
+export CLIENT_ID
+export SERVER_ID
+
 # Use the retrieved values in your application setup/configuration
 echo "Configuring application with BOT_TOKEN=$BOT_TOKEN"
+
+ls
 
 # Run npm install to install dependencies
 npm install
